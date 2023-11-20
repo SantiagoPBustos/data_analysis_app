be77624fe52d51281821a93eeec610324968b3a3
Santiago <david.pina@uptc.edu.co>
2023-11-20 09:52:05 -0500

    Configuration file xlsx

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   assetsInclude: ["**/*.xlsx"], // xlsx file should be treated as assets
   plugins: [react()],
